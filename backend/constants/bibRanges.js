884e38c6fdf6237dce6a6928089f2dda17b58c14
Abhishek Jaiswal <abhishekjaiswal68774@gmail.com>
2026-04-08 19:43:39 +0530

    refactor(constants): deprecate bibRanges in favor of excel sync

--- backend/constants/bibRanges.js
+++ backend/constants/bibRanges.js
@@ -1,10 +1,10 @@
-const BIB_RANGES = {
-    "5k": { start: 1001, end: 1999, prefix: "" },
-    "10k": { start: 2001, end: 3999, prefix: "" },
-    "21k": { start: 4001, end: 5999, prefix: "" },   // 21.097K
-    "35k": { start: 6001, end: 6999, prefix: "" },   // 35K Ultra
-    "42k": { start: 7001, end: 7999, prefix: "" },  // 42K Full Marathon
-    "charity": { start: 1001, end: 1999, prefix: "C" }
-};
+// const BIB_RANGES = {
+//     "5k": { start: 1001, end: 1999, prefix: "" },
+//     "10k": { start: 2001, end: 3999, prefix: "" },
+//     "21k": { start: 4001, end: 5999, prefix: "" },   // 21.097K
+//     "35k": { start: 6001, end: 6999, prefix: "" },   // 35K Ultra
+//     "42k": { start: 7001, end: 7999, prefix: "" },  // 42K Full Marathon
+//     "charity": { start: 1001, end: 1999, prefix: "C" }
+// };
 
-module.exports = BIB_RANGES;
+// module.exports = BIB_RANGES;
