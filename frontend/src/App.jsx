970dbc4b9603a7124919f5b965078ef0919ad6dd
Abhishek Jaiswal <abhishekjaiswal68774@gmail.com>
2026-06-04 15:06:45 +0530

    Live: Registration is been live

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -60,7 +60,7 @@ function App() {
                             <Route path="/test-camera" element={<CameraTest />} />
                             <Route
                                 path="/register"
-                                element={<RegistrationClosed />}
+                                element={<Register />}
                             />
                             <Route
                                 path="/payment"
